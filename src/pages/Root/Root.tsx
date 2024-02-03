3c0924f6f8d7c39daea5a04695bb91dfb0c97d03
Stas <dark.knight22832@gmail.com>
2024-02-03 22:07:25 +0300

    Mobile version of nav menu created

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,10 +1,25 @@
 import styles from './Root.module.css'
 import {Outlet} from "react-router-dom";
+import NavMenu from "../../components/NavMenu/NavMenu.tsx";
+import {Home, People, Person} from '@mui/icons-material'
+import {useState} from "react";
+
 const Root = () => {
+    const[selectedIndex, setSelectedIndex] = useState(0)
     return (
         <div className={styles.rootWrapper}>
-            Root!
-            <Outlet/>
+            <NavMenu
+                labels={['Очередь', 'Сотрудники', 'Профиль']}
+                selectedIndex={selectedIndex}
+                onItemSelect={(index) => setSelectedIndex(index)}
+            >
+                <Home/>
+                <People/>
+                <Person/>
+            </NavMenu>
+            <main className={styles.content}>
+                <Outlet/>
+            </main>
         </div>
     );
 };
