5d99c04ded527e1dcb83a9972f6d408dc1b63eec
Stas <dark.knight22832@gmail.com>
2024-02-07 10:25:39 +0300

    Add user menu form validation is added

--- src/pages/users_list_page/components/AddUserMenu/AddUserMenu.tsx
+++ src/pages/users_list_page/components/AddUserMenu/AddUserMenu.tsx
@@ -2,6 +2,9 @@ import {Button, Checkbox, Dialog, DialogContent, DialogTitle} from "@mui/materia
 import styles from './AddUserMenu.module.css'
 import IconTextField from "../../../../components/IconInput/IconTextField.tsx";
 import {Email, People} from "@mui/icons-material";
+import {useState} from "react";
+import {Role} from "../../../../context/auth.context.ts";
+import {validateEmail} from "../../../../utils/validateEmail.ts";
 
 export interface AddUserMenuProps {
     show: boolean,
@@ -9,26 +12,104 @@ export interface AddUserMenuProps {
 }
 
 const AddUserMenu = ({show, onClose}: AddUserMenuProps) => {
+
+    const [email, setEmail] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const updateEmail = (value: string) => {
+        setEmailError('')
+        setEmail(value)
+    }
+
+    const [firstName, setFirstName] = useState('')
+    const [firstNameError, setFirstNameError] = useState('')
+    const updateFirstName = (value: string) => {
+        setFirstNameError('')
+        setFirstName(value)
+    }
+
+    const [secondName, setSecondName] = useState('')
+    const [secondNameError, setSecondNameError] = useState('')
+    const updateSecondName = (value: string) => {
+        setSecondNameError('')
+        setSecondName(value)
+    }
+
+    const [userRoles, setUserRoles] = useState<Role[]>(['user'])
+    const [userRolesError, setUserRolesError] = useState('')
+    const toggleRole = (role: Role) => {
+        setUserRolesError('')
+        if (userRoles.includes(role)) {
+            setUserRoles(userRoles.filter((value) => value !== role))
+        } else {
+            setUserRoles([...userRoles, role])
+        }
+    }
+
+    const registerUser = () => {
+        if (!email) {
+            setEmailError('Введите почту!')
+        } else if (!validateEmail(email)) {
+            setEmailError('Неверный формат почты!')
+        }
+        if (!firstName) {
+            setFirstNameError('Введите имя!')
+        }
+        if (!secondName) {
+            setSecondNameError('Введите фамилию!')
+        }
+        if (userRoles.length === 0) {
+            setUserRolesError('У пользователя должна быть хотя бы одна роль!')
+        }
+        if (!emailError && !firstNameError && !secondNameError && !userRolesError) {
+            console.log('Ошибки нет!');
+        }
+    }
+
     return (
         <div className={styles.dialog}>
             <Dialog
                 open={show}
                 onClose={() => onClose()}
-                PaperProps={{ sx: {
-                    borderRadius: "20px",
-                    padding: "10px"
-                } }}
+                PaperProps={{
+                    sx: {
+                        borderRadius: "20px",
+                        padding: "10px"
+                    }
+                }}
             >
                 <DialogTitle className={styles.title}>Регистрация пользователя</DialogTitle>
                 <DialogContent className={styles.content}>
                     <div className={styles.form}>
-                        <IconTextField label="Введите почту" startIcon={<Email/>}/>
-                        <IconTextField label="Введите имя" startIcon={<People/>}/>
-                        <IconTextField label="Введите фамилию" startIcon={<People/>}/>
+                        <IconTextField
+                            label="Введите почту"
+                            startIcon={<Email/>}
+                            value={email}
+                            error={!!emailError}
+                            onChange={(value) => updateEmail(value)}
+                            helperText={emailError}
+                        />
+                        <IconTextField
+                            label="Введите имя"
+                            startIcon={<People/>}
+                            value={firstName}
+                            error={!!firstNameError}
+                            onChange={(value) => updateFirstName(value)}
+                            helperText={firstNameError}
+                        />
+                        <IconTextField
+                            label="Введите фамилию"
+                            value={secondName}
+                            error={!!secondNameError}
+                            onChange={(value) => updateSecondName(value)}
+                            helperText={secondNameError}
+                            startIcon={<People/>}
+                        />
                         <div className={styles.label}>Роли нового пользователя:</div>
                         <div className={styles.checkBoxContainer}>
                             <Checkbox
                                 className={styles.checkBox}
+                                checked={userRoles.includes('admin')}
+                                onChange={() => toggleRole('admin')}
                                 sx={{
                                     '&.Mui-checked': {
                                         color: "var(--primary-blue)",
@@ -40,7 +121,8 @@ const AddUserMenu = ({show, onClose}: AddUserMenuProps) => {
                         <div className={styles.checkBoxContainer}>
                             <Checkbox
                                 className={styles.checkBox}
-                                defaultChecked
+                                checked={userRoles.includes('user')}
+                                onChange={() => toggleRole('user')}
                                 sx={{
                                     '&.Mui-checked': {
                                         color: "var(--primary-blue)",
@@ -49,11 +131,12 @@ const AddUserMenu = ({show, onClose}: AddUserMenuProps) => {
                             />
                             Пользователь очереди
                         </div>
+                        {userRolesError && <div className={styles.errorMessage}>{userRolesError}</div>}
                         <div className={styles.buttonsSection}>
                             <Button onClick={() => onClose()}>
                                 Закрыть
                             </Button>
-                            <Button style={{flex: 1}}>
+                            <Button style={{flex: 1}} onClick={() => registerUser()}>
                                 Зарегистрировать
                             </Button>
                         </div>
