d6ca73e7ef90f8e9e94c7513ff66051c49ff2404
Stas <dark.knight22832@gmail.com>
2024-02-09 11:03:50 +0300

    User profile page layout is added

--- src/pages/user_profile_page/UserProfilePage.tsx
+++ src/pages/user_profile_page/UserProfilePage.tsx
@@ -1,7 +1,44 @@
+import styles from './UserProfilePage.module.css'
+import {useUser} from "../../hooks/useUser.ts";
+import UserAvatar from "../../components/UserAvatar/UserAvatar.tsx";
+import {getUserRolesString} from "../../data/dto/userInfo.dto.ts";
+import LoadingButton from "../../components/LoadingButton/LoadingButton.tsx";
+import {Button} from "@mui/material";
+
 const UserProfilePage = () => {
+    const user = useUser()
+
     return (
-        <div>
-            User profile page!
+        <div className={styles.pageWrapper}>
+            <div className={styles.title}>
+                Личный кабинет
+            </div>
+            <div className={styles.userInfo}>
+                <div className={styles.userNameSection}>
+                    <UserAvatar avatarPath={user.avatar}/>
+                    Рады вас видеть, {user.firstName}
+                </div>
+                <div className={styles.card}>
+                    <div>
+                        Ваша почта: {user.email}
+                    </div>
+                    <div>
+                        Ваши роли: {getUserRolesString(user)}
+                    </div>
+                    {!user.changedPassword &&
+                        <div className={styles.warningText}>
+                            Настоятельно рекомендуем вам сменить сгенерированный пароль
+                            на новый!
+                        </div>
+                    }
+                </div>
+                <LoadingButton loading={false}>
+                    Сменить пароль
+                </LoadingButton>
+                <Button>
+                    Выйти
+                </Button>
+            </div>
         </div>
     );
 };
