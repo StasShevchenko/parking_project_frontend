c9c1d8563a5d0559fabe1f6c696d78ef0c0a6282
Stas <dark.knight22832@gmail.com>
2024-02-10 18:27:53 +0300

    Page switch component is added

--- src/pages/home_page/HomePage.tsx
+++ src/pages/home_page/HomePage.tsx
@@ -1,15 +1,35 @@
-import {AuthContext} from "../../context/auth.context.ts";
-import {useContext} from "react";
+import styles from './HomePage.module.css'
+import PageStateWrapper from "../../components/PageStateWrapper/PageStateWrapper.tsx";
+import PageSwitcher from "./components/PageSwitcher/PageSwitcher.tsx";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import {useState} from "react";
 
 const HomePage = () => {
-
-    const user = useContext(AuthContext).authState.user
-    console.log(user)
+    const [tabIndex, setTabIndex] = useState(0)
+    const isTablet = useMediaQuery('(max-width: 880px)')
 
     return (
-        <div>
-            This is home page!
-        </div>
+        <PageStateWrapper isLoading={false}>
+            <div className={styles.pageWrapper}>
+                {isTablet ? (
+                        <>
+                            <PageSwitcher
+                                index={tabIndex}
+                                onTabClicked={(index) => setTabIndex(index)}
+                            />
+                            <div className={styles.pageContent}>
+                                {tabIndex === 0 ?
+                                    <div>0</div> :
+                                    <div>1</div>
+                                }
+                            </div>
+                        </>
+                    ) :
+                    <div className={styles.desktopPageContent}>
+
+                    </div>}
+            </div>
+        </PageStateWrapper>
     );
 };
 
