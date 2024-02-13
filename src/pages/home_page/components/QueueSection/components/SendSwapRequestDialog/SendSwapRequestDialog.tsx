622cae64e7b6942a793733ede0c9678defa5aa33
Stas <dark.knight22832@gmail.com>
2024-02-13 10:33:35 +0300

    Send swap request functionality is added

--- src/pages/home_page/components/QueueSection/components/SendSwapRequestDialog/SendSwapRequestDialog.tsx
+++ src/pages/home_page/components/QueueSection/components/SendSwapRequestDialog/SendSwapRequestDialog.tsx
@@ -3,8 +3,13 @@ import AlertDialog from "../../../../../../components/AlertDialog/AlertDialog.ts
 import {UserInPeriodDto} from "../../../../../../data/dto/userInPeriod.dto.ts";
 import dayjs from "dayjs";
 import {UserInfoDto} from "../../../../../../data/dto/userInfo.dto.ts";
-import {Button} from '@mui/material';
+import {Alert, Button} from '@mui/material';
 import LoadingButton from "../../../../../../components/LoadingButton/LoadingButton.tsx";
+import {useApi} from "../../../../../../hooks/useApi.ts";
+import {SwapApi} from "../../../../../../data/swap.api.ts";
+import {useMutation} from "@tanstack/react-query";
+import {SendSwapRequestDto} from "../../../../../../data/dto/sendSwapRequest.dto.ts";
+import {useState} from "react";
 
 export interface SendSwapRequestDialogProps {
     sender: UserInfoDto
@@ -14,6 +19,23 @@ export interface SendSwapRequestDialogProps {
 }
 
 const SendSwapRequestDialog = ({onClose, sender, receiver, swapDate}: SendSwapRequestDialogProps) => {
+    const swapApi = useApi(SwapApi)
+
+    const sendSwapMutation = useMutation({
+        mutationFn: ({dto}: { dto: SendSwapRequestDto }) => swapApi.sendSwapRequest(dto),
+        onSuccess: () => {
+            setIsSendingError(false)
+            setIsRequestSent(true)
+        },
+        onError: () => {
+            setIsSendingError(true)
+            setIsRequestSent(false)
+        }
+    })
+
+    const [isRequestSent, setIsRequestSent] = useState(false)
+    const [isSendingError, setIsSendingError] = useState(false)
+
     return (
         <AlertDialog onClose={onClose} title="Запрос на обмен">
             <div className={styles.swapRequestWrapper}>
@@ -21,17 +43,46 @@ const SendSwapRequestDialog = ({onClose, sender, receiver, swapDate}: SendSwapRe
                 {` c ${receiver.firstName} ${receiver.secondName}`} ({dayjs(swapDate).format('DD.MM.YYYY')})?
                 <div className={styles.buttonsSection}>
                     <Button
-                        onMouseDown={(event) =>event.stopPropagation()}
+                        onMouseDown={(event) => event.stopPropagation()}
                         onClick={(event) => {
                             event.stopPropagation()
                             onClose()
                         }}>
                         Закрыть
                     </Button>
-                    <LoadingButton loading={false}>
+                    <LoadingButton
+                        onMouseDown={(event) => {
+                            event.stopPropagation()
+                        }}
+                        onClick={(event) => {
+                            event.stopPropagation()
+                            sendSwapMutation.mutate({
+                                dto: {
+                                    senderId: sender.id,
+                                    receiverId: receiver.id
+                                }
+                            })
+                        }}
+                        loading={sendSwapMutation.isPending}>
                         Подтвердить
                     </LoadingButton>
                 </div>
+                {isRequestSent && <Alert
+                    severity="success"
+                    onClose={(event) => {
+                        event.stopPropagation()
+                        setIsRequestSent(false)
+                    }}>
+                    Запрос успешно отправлен!
+                </Alert>}
+                {isSendingError &&
+                    <Alert
+                        severity="error"
+                        onClose={(event) => {
+                            event.stopPropagation()
+                            setIsSendingError(false)
+                        }}>При отправке запроса произошла ошибка!
+                    </Alert>}
             </div>
         </AlertDialog>
     );
